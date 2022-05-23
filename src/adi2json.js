040f801ab42c53548991f0216b1c180e537a4192
Randy Loeb <githubrandyloeb@randonloeb.com>
2022-05-23 00:38:10 -0400

    poll crz

--- src/adi2json.js
+++ src/adi2json.js
@@ -1,4 +1,7 @@
 import { Adi2JsonArrayGetter } from './Adi2JsonArrayGetter.js'
+import Creds from './qrz.js'
+import axios from 'axios'
+import xml2js from 'xml2js'
 const fileName = './src/kn4yrm.255557.20220522041446.adi'
 
 const ary = Adi2JsonArrayGetter.getArray(fileName)
@@ -19,3 +22,24 @@ const noComments = neverFlQso.filter((record) => !ary.some(x => x.call === recor
 console.log(`no comments:${noComments.length}`)
 
 // now I could take that array and query qrz.com for addresses
+const creds = new Creds()
+const url = `https://xmldata.qrz.com/xml/current/?username=${creds.l};password=${creds.p};agent=randy1.0`
+const getKey = async () => {
+  const response = await axios.get(url)
+  const xml = response.data
+  const xmlResult = await xml2js.parseStringPromise(xml)
+  return xmlResult.QRZDatabase.Session[0].Key[0]
+}
+
+const apiKey = await getKey()
+const callLookupUrl = `https://xmldata.qrz.com/xml/current/?s=${apiKey};callsign=KN4YRM`
+
+const callLookup = async () => {
+  const response = await axios.get(callLookupUrl)
+  const xml = response.data
+  const xmlResult = await xml2js.parseStringPromise(xml)
+  return xmlResult
+}
+
+const callObject = await callLookup()
+console.log(JSON.stringify(callObject))
